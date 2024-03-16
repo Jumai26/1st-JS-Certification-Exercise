85389b4ca4b523ab8a4aaa2feb6a7de64ea5fee6
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-03-16 03:26:40 +0100

    updated javascript part

---
+++ index.js
@@ -0,0 +1,27 @@
+const myInput = document.getElementById("text-input");
+const submitButton = document.getElementById("checkin-btn");
+const resultText = document.getElementById("result");
+
+
+function check() {
+    const inputValue = myInput.value; //To get the value of the inputed text
+    const regexm = /[*\s\dW_-]/g; // To get have only alphabets
+    const ans = inputValue.replace(regexm, "").toLowerCase().split(""); //Tp convert to lovercase and array
+    const array = ans;
+
+    let isPalimdrome = true;
+    for (let i = 0; i < array.length / 2; i++) {
+        if (array[i] !== array[array.length - 1 - i]) {
+            isPalimdrome = false;
+            break;
+        } 
+    }
+
+    if (isPalimdrome) {
+        resultText.innerHTML = `${myInput.value} is a Palindrome`;
+    } else {
+        resultText.innerHTML = `${myInput.value} is not a Palindrome`;
+    }
+}
+
+submitButton.addEventListener("click", check);
